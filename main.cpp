a522910be3c3d522208e4429de7cfafb6d1f5222
Sriram-Y <sriramy2001@gmail.com>
2025-11-01 23:46:12 -0700

    THIS IS A GOOD BASELINE, TESTED SEVERAL TIMES! IT WORKS, REVERT HERE TO HW BASELINE TEST!

--- main.cpp
+++ main.cpp
@@ -4,18 +4,20 @@ void blink_led(int pinNum);
 
 int main()
 {
+    const int pinNum = 22;
+
+    gpio_init(pinNum);
+    gpio_set_dir(pinNum, GPIO_OUT);
+
     while (true)
     {
-        blink_led(15);
+        blink_led(pinNum);
     }
 }
 
 void blink_led(int pinNum)
 {
-    const uint BLINK_DELAY_MS = 10000;
-    gpio_init(pinNum);
-
-    gpio_set_dir(pinNum, GPIO_OUT);
+    const uint BLINK_DELAY_MS = 1000;
 
     gpio_put(pinNum, true);
     sleep_ms(BLINK_DELAY_MS);
